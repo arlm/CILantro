8655ec1f64c750963c06cc9e6a2bb5e917e2508b
kamil <kamil@KK-MSI>
2017-11-27 02:23:37 +0100

    First test program - C# Hello World.

---
+++ CSharpTestPrograms/Basics/CS_Basics_HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CS_Basics_HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world!");
+        }
+    }
+}

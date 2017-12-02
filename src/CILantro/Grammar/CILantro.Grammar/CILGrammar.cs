d467c62751141669de12439c284f632c7f9541a4
kamil <kamil@KK-MSI>
2017-12-02 17:33:17 +0100

    CILGrammar added to CILantro solution.

---
+++ src/CILantro/Grammar/CILantro.Grammar/CILGrammar.cs
@@ -0,0 +1,13 @@
+﻿using Irony.Parsing;
+
+namespace CILantro.Grammar
+{
+    public class CILGrammar : Irony.Parsing.Grammar
+    {
+        public CILGrammar()
+        {
+            Root = new NonTerminal("TEST");
+            Root.Rule = "TEST";
+        }
+    }
+}
--- src/CILantro/Parser/CILantro.Parser/CILParser.cs
+++ src/CILantro/Parser/CILantro.Parser/CILParser.cs
@@ -1,12 +1,28 @@
 ﻿using CILantro.AST;
+using CILantro.Grammar;
+using System;
+using System.Linq;
 
 namespace CILantro.Parser
 {
     public class CILParser
     {
+        private readonly Irony.Parsing.Parser _ironyParser;
+
+        public CILParser()
+        {
+            _ironyParser = new Irony.Parsing.Parser(new CILGrammar());
+        }
+
         public CILProgramTree Parse(string sourceCode)
         {
-            return new CILProgramTree();
+            var parseTree = _ironyParser.Parse(sourceCode);
+            if(parseTree.Status == Irony.Parsing.ParseTreeStatus.Parsed)
+            {
+                return new CILProgramTree();
+            }
+
+            throw new ArgumentException(parseTree.ParserMessages.First().Message);
         }
     }
 }

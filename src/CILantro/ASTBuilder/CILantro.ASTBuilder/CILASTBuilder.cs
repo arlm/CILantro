9557cf74125b4ac139409b7d536649820441f683
kamil <kamil@KK-MSI>
2017-12-10 02:22:58 +0100

    CILASTBuilder class added.

---
+++ src/CILantro/ASTBuilder/CILantro.ASTBuilder/CILASTBuilder.cs
@@ -0,0 +1,13 @@
+﻿using CILantro.AST;
+using Irony.Parsing;
+
+namespace CILantro.ASTBuilder
+{
+    public class CILASTBuilder
+    {
+        public CILProgramTree BuildAST(ParseTree parseTree)
+        {
+            return new CILProgramTree();
+        }
+    }
+}
--- src/CILantro/Parser/CILantro.Parser/CILParser.cs
+++ src/CILantro/Parser/CILantro.Parser/CILParser.cs
@@ -1,4 +1,5 @@
 ﻿using CILantro.AST;
+using CILantro.ASTBuilder;
 using CILantro.Extensions.Irony;
 using CILantro.Grammar;
 using System;
@@ -9,10 +10,12 @@ namespace CILantro.Parser
     public class CILParser
     {
         private readonly Irony.Parsing.Parser _ironyParser;
+        private readonly CILASTBuilder _astBuilder;
 
         public CILParser()
         {
             _ironyParser = new Irony.Parsing.Parser(new CILGrammar());
+            _astBuilder = new CILASTBuilder();
         }
 
         public CILProgramTree Parse(string sourceCode)
@@ -20,7 +23,8 @@ namespace CILantro.Parser
             var parseTree = _ironyParser.Parse(sourceCode);
             if(parseTree.Status == Irony.Parsing.ParseTreeStatus.Parsed)
             {
-                return new CILProgramTree();
+                var programTree = _astBuilder.BuildAST(parseTree);
+                return programTree;
             }
 
             throw new ArgumentException(parseTree.ParserMessages.First().UserFriendlyMessage());
